9948fc5456bdf5244c35766ee28a5ea2109d0372
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-23 11:51:07 +0100

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const closeBtn = document.querySelector(".close-btn");
+const toggleBtn = document.querySelector('.sidebar-toggle')
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener('click', function () {
+    sidebar.classList.toggle("show-sidebar")
+})
+
+closeBtn.addEventListener('click', function () {
+    sidebar.classList.remove("show-sidebar")
+})
